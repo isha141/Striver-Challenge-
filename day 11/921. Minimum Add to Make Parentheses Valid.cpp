b2eb85b3c67e7e373e0ab307531ba9edf54fd309
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-10-17 17:58:44 +0530

    Create 921. Minimum Add to Make Parentheses Valid.cpp

---
+++ day 11/921. Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int count=0;
+        for(auto itr: s){
+            if(itr==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }
+                else{
+                   count+=1;
+                }
+            }
+            else{
+                st.push(itr);
+            }
+        }
+         return count+st.size();
+    }
+};
+// T.C=O(n)
+// S.C=o(n)
